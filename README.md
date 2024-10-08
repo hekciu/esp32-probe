b9f97dc6d11a838a2e3ccce53d5c9aece118f246
hekciu <jakublasecki321@gmail.com>
2024-10-09 00:26:13 +0200

    created small working example with full configuration

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-## ESP32-S3 probe
+## Playing aroung with ESP32-S3
+# OS: Debian 12
+
+# In examples below PORT should be replaced with actual system device eg. /dev/ttyUSB0 or /dev/ttyACM0
+
+# First time:
+
+- source ./setup.sh
+- source ./init.sh
+- idf.py set-target esp32s3
+- idf.py menuconfig
+- idf.py build
+- sudo usermod -a -G dialout $USER
+- sudo chmod a+rw PORT
+
+# Next times:
+- source ./init.sh
+- idf.py build
+- idf.py -p PORT flash 
+
+# To monitor output:
+- idf.py -p PORT monitor
--- setup.sh
+++ setup.sh
@@ -1,1 +1,10 @@
-echo 'Setting up environment for esp32 programming'
+# reference: https://docs.espressif.com/projects/esp-idf/en/latest/esp32/get-started/linux-macos-setup.html
+
+echo 'Setting up environment for esp32 programming [DEBIAN]'
+sudo apt-get install git wget flex bison gperf python3 python3-pip python3-venv cmake ninja-build ccache libffi-dev libssl-dev dfu-util libusb-1.0-0
+mkdir -p ~/esp
+cd ~/esp
+git clone --recursive https://github.com/espressif/esp-idf.git
+cd ~/esp/esp-idf
+./install.sh esp32,esp32s3
+
